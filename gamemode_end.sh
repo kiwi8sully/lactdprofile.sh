7807f320a09abd0690efbd7f207727e91e57af89
fluffy <109002399+kiwi8sully@users.noreply.github.com>
2024-09-29 19:31:58 +1300

    refactor gamemode_end.sh for lactd profiles

--- gamemode_end.sh
+++ gamemode_end.sh
@@ -1,38 +1,6 @@
 #!/bin/bash
 
-#echo '{"command": "list_devices" }' | ncat -U /run/lactd.sock
-#echo '{"command": "device_power_profile_modes", "args": { "id":  "1002:731F-1DA2:E409-0000:28:00.0" } }' | ncat -U /run/lactd.sock
-#echo '{"command": "set_clocks_value", "args": { "id":  "1002:731F-1DA2:E409-0000:28:00.0", "command":{"type":"reset" }}}' | ncat -U /run/lactd.sock
-#echo '{"command": "confirm_pending_config", "args": { "command": "confirm" } }' | ncat -U /run/lactd.sock
-
-ID=$( echo '{"command": "list_devices" }' | ncat -U /run/lactd.sock | cut -d , -f 2 | cut -d \" -f 6 )
-
-confirm_pending_config() {
-    echo '{"command": "confirm_pending_config", "args": { "command": "confirm" } }' | ncat -U /run/lactd.sock
-}
-
-set_power_profile_mode() {
-    case "$1" in
-    [0-6])
-        echo '{"command":"set_performance_level","args":{"id":"'"$ID"'","performance_level":"manual"}}' | ncat -U /run/lactd.sock
-        confirm_pending_config
-        echo '{"command":"set_power_profile_mode","args":{"id":"'"$ID"'","index":'$1'}}' | ncat -U /run/lactd.sock
-        confirm_pending_config
-        ;;
-    auto)
-        echo '{"command":"set_performance_level","args":{"id":"'"$ID"'","performance_level":"manual"}}' | ncat -U /run/lactd.sock
-        confirm_pending_config
-        echo '{"command":"set_power_profile_mode", "args":{"id":"'"$ID"'","index":null}}' | ncat -U /run/lactd.sock
-        confirm_pending_config
-        echo '{"command":"set_performance_level","args":{"id":"'"$ID"'","performance_level":"auto"}}' | ncat -U /run/lactd.sock
-        confirm_pending_config
-        ;;
-    *)
-        echo "You need to specify a power profile [0-6] | auto"
-        ;;
-    esac
-}
-
-set_power_profile_mode auto
+$LACTD_PROFILE="BootUp"
+echo '{"command":"set_profile","args":{"name":"'"$LACTD_PROFILE"'"}}' | ncat -U /run/lactd.sock
 
 exit 0
