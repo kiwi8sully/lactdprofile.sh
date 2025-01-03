51c8d7af087991f65d9a3c27dbb0bec221f25099
fluffy <109002399+kiwi8sully@users.noreply.github.com>
2025-01-03 16:01:05 +1300

    Added create/move/delete profile functions

--- gamemode_end.sh
+++ gamemode_end.sh
@@ -2,5 +2,5 @@
 
 LACTD_PROFILE=BootUp
 echo '{"command":"set_profile","args":{"name":"'"$LACTD_PROFILE"'","auto_switch":false}}' | ncat -U /run/lactd.sock
-
+#echo '{"command":"set_profile","args":{"name":null,"auto_switch":false}}' | ncat -U /run/lactd.sock
 exit 0
